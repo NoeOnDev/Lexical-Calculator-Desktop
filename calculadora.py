456972d45cbfc7ba47de1115d53684a217d0993e
NoeAlejandroRodriguezMoto <alxg5516@gmail.com>
2024-03-14 16:07:48 -0600

    Agregado análisis léxico para la calculadora

--- calculadora.py
+++ calculadora.py
@@ -1,5 +1,25 @@
 import tkinter as tk
 from tkinter import messagebox
+import ply.lex as lex
+
+tokens = ('NUMBER', 'PLUS', 'MINUS', 'TIMES', 'DIVIDE')
+t_PLUS = r'\+'
+t_MINUS = r'-'
+t_TIMES = r'\*'
+t_DIVIDE = r'/'
+
+def t_NUMBER(t):
+    r'\d+'
+    t.value = int(t.value)
+    return t
+
+t_ignore = ' \t'
+
+def t_error(t):
+    print(f"Illegal character '{t.value[0]}'")
+    t.lexer.skip(1)
+
+lexer = lex.lex()
 
 def calcular(operacion):
     try:
